f83e43cbd805d507aeee1f661c13bd22803e4538
Passion4Coding <qw4151@gmail.com>
2021-05-03 10:23:12 +0530

    API modified

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -11,14 +11,9 @@ export class MoviesService {
   url: any = 'http://www.omdbapi.com';
   apikey = 'bce55a7a';
 
-  loadData() {
-    // return this.http.get( "http://34.73.45.188:4500/api/getPolicy?type=id");
-    return this.http.get(
-      'http://www.omdbapi.com/?t=Star Wars&plot=full&apikey=bce55a7a'
-    );
-  }
-
   getMovie(movieName: any): Observable<any> {
-    return this.http.get(this.url + `?t=${movieName}&` + `apikey=${this.apikey}`);
+    return this.http.get(
+      this.url + `?t=${movieName}&` + `apikey=${this.apikey}`
+    );
   }
 }
